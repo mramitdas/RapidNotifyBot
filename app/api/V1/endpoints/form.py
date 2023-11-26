08bd48eadda8e98eb0785b66f98c3a58241ddc81
amit <mramitdas99@gmail.com>
2023-11-26 22:48:47 +0530

    FEAT: create endpoint for notifyme
    Closes #11

---
+++ app/api/V1/endpoints/form.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, HTTPException
+
+from app.schemas.form import FormInput
+
+contact_form = APIRouter()
+
+
+@contact_form.post("/notifyme")
+async def register_form_input(data: FormInput):
+    try:
+        user_dict = data.model_dump(exclude_unset=True)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Invalid filter parameter: {e}")
+
+    return user_dict.get("data")
