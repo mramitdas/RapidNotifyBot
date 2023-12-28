322da4d29fab4a395bf752b7c80be7d959699612
amit <mramitdas99@gmail.com>
2023-12-28 14:35:07 +0530

    FEAT: added bot messages

---
+++ app/bot/info.py
@@ -0,0 +1,55 @@
+def bot_welcome(name):
+    return f"""
+👋 Welcome to NotifyMeBot!
+
+Hey {name}! 🌟
+
+Get ready to supercharge your notification experience with NotifyMeBot – your ultimate notification companion! We're not just about keeping you in the loop; we're here to elevate your notification game to new heights! 🚀
+
+🔔 **Key Features:**
+- **Instant Notifications:** Receive lightning-fast alerts on Telegram for seamless updates.
+- **Customizable Alerts:** Tailor notifications to match your preferences.
+- **Real-time Updates:** Stay connected and informed with timely notifications.
+
+✨ **How to Get Started:**
+- Use /subscribe to obtain your personalized API key and secret.
+
+🌐 **Connect with Us:**
+Have questions or brilliant suggestions? Reach out to our support team via t.me/amitdas99. Your feedback is the secret sauce in making NotifyMeBot even more awesome!
+
+🚀 **Ready to Dive In?**
+Type /help to access our command list and unveil the plethora of amazing features at your fingertips.
+
+Thank you for choosing NotifyMeBot! We're here to simplify your life and keep you organized. Happy notifying! 🚀✉️
+
+Best regards,
+The NotifyMeBot Team
+"""
+
+
+def bot_help(name):
+    return f"""
+👋 Need Help? We've Got Your Back!
+
+Hello {name}! 🌟
+
+If you ever find yourself in a bit of a pickle or just want to explore the full potential of NotifyMeBot, you're in the right place! Our support team is here to assist you every step of the way.
+
+🛠️ **How We Can Help:**
+   - **Technical Assistance:** Encountering issues? We'll troubleshoot with you.
+   - **Feature Exploration:** Learn how to make the most of NotifyMeBot's capabilities.
+   - **General Inquiries:** Have questions? We've got answers!
+
+💡 **Quick Tips:**
+   - /start - Begin your NotifyMeBot journey.
+   - /help - Access the command list and get assistance.
+   - /subscribe - Obtain your personalized API key and secret.
+
+🌐 **Connect with Us:**
+Your feedback and questions are valuable to us. Don't hesitate to reach out via t.me/amitdas99. – we're here to make your NotifyMeBot experience smooth and enjoyable!
+
+Thank you for choosing NotifyMeBot! We're committed to ensuring you have the best experience possible. Happy exploring! 🚀✉️
+
+Best regards,
+The NotifyMeBot Team
+"""
