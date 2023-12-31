30c6718a80c4d2cbb40f1a03c03894a639d507f0
amit <mramitdas99@gmail.com>
2023-12-31 20:20:35 +0530

    FEAT: added pydantic models

---
+++ app/db/validator.py
@@ -0,0 +1,42 @@
+from typing import Dict, List, Union
+
+from pydantic import BaseModel, ValidationError, validator
+
+from app.exceptions.custom_exceptions import MissingAttributeError
+
+
+class MongoDbClientConfig(BaseModel):
+    db_url: str
+
+
+class BaseInput(BaseModel):
+    db_name: str
+    table_name: str
+
+
+class UploadDataInput(BaseInput):
+    data: Dict
+    
+    @validator("data", pre=True, always=True)
+    def validate_data(cls, value):
+        if not value:
+            raise ValueError("Data cannot be blank")
+        return value
+
+
+class QueryDataInput(BaseInput):
+    filter: Dict
+    
+    @validator("data", pre=True, always=True)
+    def validate_data(cls, value):
+        if not value:
+            raise ValueError("filter cannot be blank")
+        return value
+
+
+class UpdateDataInput(BaseInput):
+    data: Dict
+
+
+class DeleteDataInput(QueryDataInput):
+    pass
